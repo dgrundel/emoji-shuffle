7fd3caec50e0d1060c0570282e59374301b66614
Daniel <daniel@grundel.local>
2024-12-24 18:17:00 -0800

    move to docs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: 'src/index.ts',
   output: {
-    dir: 'dist',
+    dir: 'docs',
     format: 'iife'
   },
   plugins: [typescript()]
